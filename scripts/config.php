bc4030f8d811df384f6fba2c2562d2fe2f1dc62e
AllCrypto <mcorrientes@me.com>
2018-02-02 20:24:33 -0600

    Added external config file.

---
+++ scripts/config.php
@@ -0,0 +1,9 @@
+<?php
+
+	// Mine some of the coins which haven't been mined for a long time.
+	// For those coins you see gaining a lot in value in the future.
+	define('DUST_COLLECT_ENABLED', TRUE);
+	define('DUST_COLLECT_START', 23);
+	define('DUST_COLLECT_END', 4);
+
+?>
