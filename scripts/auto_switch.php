de890a3c6dea4d65be69266c8d11af8c524e069b
AllCrypto <mcorrientes@me.com>
2018-02-03 23:27:00 -0800

    Implemented timed coin mining.

--- scripts/auto_switch.php
+++ scripts/auto_switch.php
@@ -6,11 +6,14 @@
 		public $dust_collect_enabled = TRUE; // Mine for specific time some of the least mined coins instead of only the most profitable
 		public $dust_collect_start = 23; // Time Range For Dust Collect ( START )
 		public $dust_collect_end = 4; // Time Range For Dust Collect ( END )
+		public $wtm_api_url = 'http://whattomine.com/coins.json?utf8=?&adapt_q_280x=0&adapt_q_380=0&adapt_q_fury=0&adapt_q_470=0&adapt_q_480=0&adapt_q_570=0&adapt_q_580=0&adapt_q_vega56=0&adapt_q_vega64=0&adapt_q_750Ti=0&adapt_q_1050Ti=0&adapt_q_10606=0&adapt_q_1070=6&adapt_1070=true&adapt_q_1070Ti=0&adapt_q_1080=13&adapt_q_1080Ti=0&eth=true&factor%5Beth_hr%5D=180.0&factor%5Beth_p%5D=720.0&grof=true&factor%5Bgro_hr%5D=201.0&factor%5Bgro_p%5D=780.0&x11gf=true&factor%5Bx11g_hr%5D=69.0&factor%5Bx11g_p%5D=720.0&cn=true&factor%5Bcn_hr%5D=3780.0&factor%5Bcn_p%5D=600.0&eq=true&factor%5Beq_hr%5D=2580.0&factor%5Beq_p%5D=720.0&lre=true&factor%5Blrev2_hr%5D=213000.0&factor%5Blrev2_p%5D=780.0&ns=true&factor%5Bns_hr%5D=6000.0&factor%5Bns_p%5D=780.0&lbry=true&factor%5Blbry_hr%5D=1620.0&factor%5Blbry_p%5D=720.0&bk2bf=true&factor%5Bbk2b_hr%5D=9600.0&factor%5Bbk2b_p%5D=720.0&bk14=true&factor%5Bbk14_hr%5D=14400.0&factor%5Bbk14_p%5D=750.0&pas=true&factor%5Bpas_hr%5D=5700.0&factor%5Bpas_p%5D=720.0&skh=true&factor%5Bskh_hr%5D=165.0&factor%5Bskh_p%5D=720.0&n5=true&factor%5Bn5_hr%5D=57.0&factor%5Bn5_p%5D=345.0&factor%5Bl2z_hr%5D=420.0&factor%5Bl2z_p%5D=300.0&factor%5Bcost%5D=0.1&sort=Profit&volume=0&revenue=current&factor%5Bexchanges%5D%5B%5D=&factor%5Bexchanges%5D%5B%5D=abucoins&factor%5Bexchanges%5D%5B%5D=bitfinex&factor%5Bexchanges%5D%5B%5D=bittrex&factor%5Bexchanges%5D%5B%5D=bleutrade&factor%5Bexchanges%5D%5B%5D=cryptopia&factor%5Bexchanges%5D%5B%5D=hitbtc&factor%5Bexchanges%5D%5B%5D=poloniex&factor%5Bexchanges%5D%5B%5D=yobit&dataset=Main&commit=Calculate';
+		public $coin_timers;
 		
 		private $home_path = '';
 		private $ethos_path = '';
 		
 		private $coins;
+		private $timers;
 		
 		function __construct()
 		{
@@ -28,6 +31,15 @@
 				return;
 			
 			$this->load_coins();
+			$this->load_timers();
+			
+			// Coins for specific time set?
+			if($this->timers)
+			{
+				$this->timer_switch();
+				return;
+			}
+			
 			$hour = date('H', time());
 			// Mine least mined coins ( e.g. between 12 - 16 ) -- Dust Collection
 			if($this->dust_collect_enabled && ($hour >= $this->dust_collect_start && $hour < $this->dust_collect_end))
@@ -39,7 +51,7 @@
 		/// Switch to the most profitable coin
 		private function profit_switch()
 		{
-			$json_coins = file_get_contents('http://whattomine.com/coins.json?utf8=✓&adapt_q_280x=0&adapt_q_380=0&adapt_q_fury=0&adapt_q_470=0&adapt_q_480=0&adapt_q_570=0&adapt_q_580=0&adapt_q_vega56=0&adapt_q_vega64=0&adapt_q_750Ti=0&adapt_q_1050Ti=0&adapt_q_10606=0&adapt_q_1070=6&adapt_1070=true&adapt_q_1070Ti=0&adapt_q_1080=13&adapt_q_1080Ti=0&eth=true&factor%5Beth_hr%5D=180.0&factor%5Beth_p%5D=720.0&grof=true&factor%5Bgro_hr%5D=201.0&factor%5Bgro_p%5D=780.0&x11gf=true&factor%5Bx11g_hr%5D=69.0&factor%5Bx11g_p%5D=720.0&cn=true&factor%5Bcn_hr%5D=3780.0&factor%5Bcn_p%5D=600.0&eq=true&factor%5Beq_hr%5D=2580.0&factor%5Beq_p%5D=720.0&lre=true&factor%5Blrev2_hr%5D=213000.0&factor%5Blrev2_p%5D=780.0&ns=true&factor%5Bns_hr%5D=6000.0&factor%5Bns_p%5D=780.0&lbry=true&factor%5Blbry_hr%5D=1620.0&factor%5Blbry_p%5D=720.0&bk2bf=true&factor%5Bbk2b_hr%5D=9600.0&factor%5Bbk2b_p%5D=720.0&bk14=true&factor%5Bbk14_hr%5D=14400.0&factor%5Bbk14_p%5D=750.0&pas=true&factor%5Bpas_hr%5D=5700.0&factor%5Bpas_p%5D=720.0&skh=true&factor%5Bskh_hr%5D=165.0&factor%5Bskh_p%5D=720.0&n5=true&factor%5Bn5_hr%5D=57.0&factor%5Bn5_p%5D=345.0&factor%5Bl2z_hr%5D=420.0&factor%5Bl2z_p%5D=300.0&factor%5Bcost%5D=0.1&sort=Profit&volume=0&revenue=current&factor%5Bexchanges%5D%5B%5D=&factor%5Bexchanges%5D%5B%5D=abucoins&factor%5Bexchanges%5D%5B%5D=bitfinex&factor%5Bexchanges%5D%5B%5D=bittrex&factor%5Bexchanges%5D%5B%5D=bleutrade&factor%5Bexchanges%5D%5B%5D=cryptopia&factor%5Bexchanges%5D%5B%5D=hitbtc&factor%5Bexchanges%5D%5B%5D=poloniex&factor%5Bexchanges%5D%5B%5D=yobit&dataset=Main&commit=Calculate'); //');
+			$json_coins = file_get_contents($this->wtm_api_url); //');
 			$data_coins = json_decode($json_coins, true);
 			$profits = FALSE;
 
@@ -106,6 +118,41 @@
 				$this->write_log(date('m/d/Y H:i:s')." - Switched to $new_coin (Dust Collect)\r\n");
 		}
 		
+		private function timer_switch()
+		{
+			$current_coin = file_get_contents($this->home_path.'scripts/current_coin.txt');
+			unset($this->timers[$current_coin]); // Remove the current active coin from list
+			
+			////////
+			//// Get the least mined coin
+			
+			$coin_distance = array();
+			$log = file($this->home_path.'scripts/log');
+			$log = array_reverse($log); // Sort: Newest Log To Oldest
+			
+			foreach($this->timers as $COIN => $COIN_VALUE)
+			{
+				$coin_distance[$COIN] = 10000; // Unmined coins start the furthest away
+				
+				// Find distance since last mine
+				for($i = 0; $i<count($log); $i++)
+				{
+					if(strpos($log[$i], ' '.$COIN.' '))
+					{
+						$coin_distance[$COIN] = $i; // Found distance for coin
+						break;
+					}
+				}
+			}
+			uasort($coin_distance, array($this, 'cb_float_rsort')); // Sort by distance
+			
+			$new_coin = key($coin_distance);
+			if($new_coin === NULL) // Nothing to do.. No new coin found..
+				return;
+			if($this->switch_coin($new_coin))
+				$this->write_log(date('m/d/Y H:i:s')." - Switched to $new_coin (TIMED COIN)\r\n");
+		}
+		
 		// Load all configs ( Coins )
 		private function load_coins()
 		{
@@ -122,7 +169,32 @@
 				if(strpos($tag, '.'))
 					$tag = substr($tag, 0, strpos($tag, '.'));
 				
-				$this->coins[strtoupper($tag)] = array('config' => $filename);
+				$this->coins[strtoupper($tag)] = array('config' => $filename, 'folder_path' => '');
+			}
+		}
+		
+		// Load coins that are set for specifc times
+		private function load_timers()
+		{
+			$this->timers = FALSE;
+			foreach($this->coin_timers as $timer)
+			{
+				if(!is_numeric($timer['START_HOUR']) || !is_numeric($timer['END_HOUR'])) // Not a valid format ( not a number )
+					continue;
+				if($timer['START_HOUR'] < 0 && $timer['START_HOUR'] > 24) // Not a valid format ( exceeds or below standard 24h format )
+					continue;
+				if($timer['END_HOUR'] < 0 && $timer['END_HOUR'] > 24) // Not a valid format ( exceeds or below standard 24h format )
+					continue;
+				
+				$current_hour = (int)date('H', time());
+				if($current_hour < $timer['START_HOUR'] || $current_hour >= $timer['END_HOUR']) // Not set for current time
+					continue;
+				
+				$tag = strtoupper($timer['COIN']);
+				if(!isset($this->coins[$tag]))
+					continue;
+				
+				$this->timers[$tag] = $timer;
 			}
 		}
 		
@@ -185,6 +257,8 @@
 	$app->dust_collect_enabled = DUST_COLLECT_ENABLED;
 	$app->dust_collect_start = DUST_COLLECT_START;
 	$app->dust_collect_end = DUST_COLLECT_END;
+	$app->wtm_api_url = WTM_API_URL;
+	$app->coin_timers = $COIN_TIMERS;
 	$app->run();
 
 ?>
--- scripts/config.php
+++ scripts/config.php
@@ -1,9 +1,22 @@
 <?php
+	//////////
+	///// Mining specific coins in specific times
+	//////////
+	$COIN_TIMERS = FALSE;
+	//$COIN_TIMERS[] = array('COIN' => 'CBS', 'START_HOUR' => 06, 'END_HOUR' => 10);
+	//$COIN_TIMERS[] = array('COIN' => 'TZC', 'START_HOUR' => 06, 'END_HOUR' => 10);
 
-	// Mine some of the coins which haven't been mined for a long time.
-	// For those coins you see gaining a lot in value in the future.
+	//////////
+	///// Mine some of the coins which haven't been mined for a long time.
+	///// For those coins you see gaining a lot in value in the future.
+	//////////
 	define('DUST_COLLECT_ENABLED', TRUE);
 	define('DUST_COLLECT_START', 23);
 	define('DUST_COLLECT_END', 4);
+	
+	//////////
+	///// API URL used for looking up current prices
+	//////////
+	define('WTM_API_URL', 'http://whattomine.com/coins.json?utf8=?&adapt_q_280x=0&adapt_q_380=0&adapt_q_fury=0&adapt_q_470=0&adapt_q_480=0&adapt_q_570=0&adapt_q_580=0&adapt_q_vega56=0&adapt_q_vega64=0&adapt_q_750Ti=0&adapt_q_1050Ti=0&adapt_q_10606=0&adapt_q_1070=6&adapt_1070=true&adapt_q_1070Ti=0&adapt_q_1080=13&adapt_q_1080Ti=0&eth=true&factor%5Beth_hr%5D=180.0&factor%5Beth_p%5D=720.0&grof=true&factor%5Bgro_hr%5D=201.0&factor%5Bgro_p%5D=780.0&x11gf=true&factor%5Bx11g_hr%5D=69.0&factor%5Bx11g_p%5D=720.0&cn=true&factor%5Bcn_hr%5D=3780.0&factor%5Bcn_p%5D=600.0&eq=true&factor%5Beq_hr%5D=2580.0&factor%5Beq_p%5D=720.0&lre=true&factor%5Blrev2_hr%5D=213000.0&factor%5Blrev2_p%5D=780.0&ns=true&factor%5Bns_hr%5D=6000.0&factor%5Bns_p%5D=780.0&lbry=true&factor%5Blbry_hr%5D=1620.0&factor%5Blbry_p%5D=720.0&bk2bf=true&factor%5Bbk2b_hr%5D=9600.0&factor%5Bbk2b_p%5D=720.0&bk14=true&factor%5Bbk14_hr%5D=14400.0&factor%5Bbk14_p%5D=750.0&pas=true&factor%5Bpas_hr%5D=5700.0&factor%5Bpas_p%5D=720.0&skh=true&factor%5Bskh_hr%5D=165.0&factor%5Bskh_p%5D=720.0&n5=true&factor%5Bn5_hr%5D=57.0&factor%5Bn5_p%5D=345.0&factor%5Bl2z_hr%5D=420.0&factor%5Bl2z_p%5D=300.0&factor%5Bcost%5D=0.1&sort=Profit&volume=0&revenue=current&factor%5Bexchanges%5D%5B%5D=&factor%5Bexchanges%5D%5B%5D=abucoins&factor%5Bexchanges%5D%5B%5D=bitfinex&factor%5Bexchanges%5D%5B%5D=bittrex&factor%5Bexchanges%5D%5B%5D=bleutrade&factor%5Bexchanges%5D%5B%5D=cryptopia&factor%5Bexchanges%5D%5B%5D=hitbtc&factor%5Bexchanges%5D%5B%5D=poloniex&factor%5Bexchanges%5D%5B%5D=yobit&dataset=Main&commit=Calculate');
 
 ?>
